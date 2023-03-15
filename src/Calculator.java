61beb9ea16eaad44fb4dd0beea4d2db2e43c5fb7
Chirag <bansalc73@gmail.com>
2023-03-15 12:33:19 +0530

    Junit running and pipleine added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,4 @@
-package main;
+package src;
 import java.util.Scanner;
 
 // import java.lang.math;
--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,7 +1,8 @@
-package test;
+// package test;
+import src.Calculator;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import main.Calculator;
+
 
 public class CalculatorTest {
     private static final double DELTA = 1e-15;
